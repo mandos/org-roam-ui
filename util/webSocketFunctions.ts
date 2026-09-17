9f7a8a973275aeaade8aba0c78f017f1ba67f304
Marek Maksimczyk <marek.maksimczyk@mandos.net.pl>
2026-09-17 13:54:32 +0900

    chore: Remove dead code

--- util/webSocketFunctions.ts
+++ util/webSocketFunctions.ts
@@ -8,10 +8,6 @@ export function sendMessageToEmacs(command: string, data: {}, webSocket: Reconne
   webSocket.send(JSON.stringify({ command: command, data: data }))
 }
 
-export function getOrgText(node: OrgRoamNode, webSocket: ReconnectingWebSocket) {
-  sendMessageToEmacs('getText', { id: node.id }, webSocket)
-}
-
 export function openNodeInEmacs(node: OrgRoamNode, webSocket: ReconnectingWebSocket) {
   sendMessageToEmacs('open', { id: node.id }, webSocket)
 }
